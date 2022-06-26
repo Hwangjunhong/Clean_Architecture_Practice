4bffbbd91429076ebbd2da15bb9bc9ad6983fcf3
Hwangjunhong <ghkdwnsghd1@naver.com>
2022-06-26 16:47:11 +0900

    Update View

--- presentation/src/main/java/com/hong/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/hong/presentation/view/MainActivity.kt
@@ -3,10 +3,15 @@ package com.hong.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hong.presentation.R
+import com.hong.presentation.base.BaseActivity
+import com.hong.presentation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
+    override fun init() {}
 }
